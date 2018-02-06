3a1aaff0b39a7ebe781fe5e88a326105adeb5a29
deostroll <deostroll@gmail.com>
2018-02-06 15:20:14 +0530

    validation check

--- index.js
+++ index.js
@@ -14,9 +14,17 @@ const feelJson = readJson('feel_program.json');
 
 ast = createDecisionGraphAST(feelJson);
 
-executeDecisionService(ast, "main", { name: "Ajay", age: 23, height: 6 , customerId: 'abc12'}).then(answers => {
-	console.log('answers:', answers);
-})
-.catch(err => {
-	console.error(err);
-})
+// executeDecisionService(ast, "main", { name: "Ajay", age: 23, height: 6 , customerId: 'abc12'}).then(answers => {
+// 	console.log('answers:', answers, typeof answers);
+// })
+// .catch(err => {
+// 	console.error(err);
+// })
+var payload = { name: "Ajay", customerId: 43223 };
+var decisions = ["main", "checks"];
+var promises = decisions.map(d => executeDecisionService(ast, d, payload));
+
+Promise.all(promises).then(results => {
+	return results.reduce((obj, ans, idx) => Object.assign(obj, {[decisions[idx]] : ans }), {});
+}).then(validation => console.log('Validation Result:', validation))
+.catch(err => console.error('Error:', err))
